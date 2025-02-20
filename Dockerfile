8affec7967f31f8d6b5a4f0d463c5d26f8c87220
anthonysfarias <akileslive.farias10@gmail.com>
2025-02-20 19:33:36 -0300

    docker: corrigindo bugs e melhorando desempenho

--- Dockerfile
+++ Dockerfile
@@ -2,12 +2,17 @@ FROM node:20-alpine
 
 WORKDIR /app
 
+# Copiar as dependências para instalar
 COPY package.json yarn.lock ./
 
+# Instalar dependências
 RUN yarn install
 
+# Copiar o restante do código
 COPY . .
 
+# Expor a porta do app
 EXPOSE 3000
 
+# Comando para iniciar o servidor
 CMD ["yarn", "run", "start"]
--- mongo/init.js
+++ mongo/init.js
@@ -1,11 +1,15 @@
 db.createUser(
 	{
 		user: "sol",
-		pwd: process.env.MONGO_PASSWORD,
+		pwd: "password",  // Senha direta ou variável de ambiente se necessário
 		roles: [
 			{
 				role: "readWrite",
 				db: "sol"
+			},
+			{
+				role: "readWrite",
+				db: "test"  // Adicionando permissão para o banco 'test'
 			}
 		]
 	}
