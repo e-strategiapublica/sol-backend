c9e1628a9805613fb8d2bd19e3da172d9cebe1fc
Gabriel Mussolini <gabrielmussolini@hotmail.com>
2025-04-03 22:09:33 -0300

    Add environment configuration and improve project structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { ScheduleModule } from '@nestjs/schedule';
-import { SolModule } from './modules/SOL/sol.module';
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { ScheduleModule } from "@nestjs/schedule";
+import { SolModule } from "./modules/SOL/sol.module";
+import AppEnviroments from "./shared/config/app.enviroments";
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: `${process.cwd()}/.env`
+      envFilePath: `${process.cwd()}/.env`,
+      load: [AppEnviroments],
     }),
     ScheduleModule.forRoot(),
     SolModule,
