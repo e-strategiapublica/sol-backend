8da0235f4c00ccfe5f6a99485e4e76804eb98d16
Gabriel Mussolini <gabrielmussolini@hotmail.com>
2025-04-07 00:46:43 -0300

    Update production start script to reference main.js and remove unused environment configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,11 @@ import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { ScheduleModule } from "@nestjs/schedule";
 import { SolModule } from "./modules/SOL/sol.module";
-import AppEnviroments from "./shared/config/app.enviroments";
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: `${process.cwd()}/.env`,
-      load: [AppEnviroments],
     }),
     ScheduleModule.forRoot(),
     SolModule,
