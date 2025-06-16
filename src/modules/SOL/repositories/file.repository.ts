cd55199d5a356270a1ac49d971c0fb9d63cf135a
Gabriel Mussolini <gabrielmussolini@hotmail.com>
2025-06-16 20:38:53 -0300

    fix/code-secutiry-2

--- src/modules/SOL/repositories/file.repository.ts
+++ src/modules/SOL/repositories/file.repository.ts
@@ -1,19 +1,40 @@
-import { Injectable, Scope, StreamableFile } from "@nestjs/common";
+import {
+  HttpStatus,
+  Injectable,
+  Logger,
+  Scope,
+  StreamableFile,
+} from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import * as fs from "fs";
 import { EnviromentVariablesEnum } from "../../../shared/enums/enviroment.variables.enum";
 import * as path from "path";
-
+import sanitizeFilename from "sanitize-filename";
+import { CustomHttpException } from "src/shared/exceptions/custom-http.exception";
 @Injectable()
 export class FileRepository {
-  constructor(private readonly _configService: ConfigService) {}
+  private bucketPath: string;
+  private readonly _logger = new Logger(FileRepository.name);
 
-  upload(filename: string, base64: string): string {
+  constructor(private readonly _configService: ConfigService) {
     const bucket = this._configService.get<string>(
       EnviromentVariablesEnum.BUCKET,
     );
+    this.bucketPath = path.resolve(bucket);
+  }
 
-    const fullPath = path.join(bucket, filename);
+  private getFilePath(filename: string): string {
+    const sanitizedFilename = sanitizeFilename(filename);
+    const fullPath = path.resolve(this.bucketPath, sanitizedFilename);
+    if (!fullPath.startsWith(this.bucketPath)) {
+      this._logger.error({ full_path: fullPath }, "invalid file path");
+      throw new CustomHttpException("", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+    return fullPath;
+  }
+
+  upload(filename: string, base64: string): string {
+    const fullPath = this.getFilePath(filename);
     const dir = path.dirname(fullPath);
 
     if (!fs.existsSync(dir)) {
