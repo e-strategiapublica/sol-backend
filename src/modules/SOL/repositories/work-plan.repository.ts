eaf2c1c3243996c172a774d4bab9cb912d0d772d
Gabriel Mussolini <gabrielmussolini@hotmail.com>
2025-06-16 21:31:57 -0300

    fix: validate ObjectId in repository methods

--- src/modules/SOL/repositories/work-plan.repository.ts
+++ src/modules/SOL/repositories/work-plan.repository.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@nestjs/common";
 import { WorkPlan } from "../schemas/work-plan.schema";
 import { WorkPlanModel } from "../models/work-plan.model";
 import { InjectModel } from "@nestjs/mongoose";
-import { Model } from "mongoose";
+import mongoose, { Model } from "mongoose";
 import { WorkPlanInterface } from "../interfaces/agreement.interface";
 import { MongoClient } from "mongodb";
 
@@ -16,18 +16,29 @@ export class WorkPlanRepository {
     @InjectModel(WorkPlan.name) private readonly _model: Model<WorkPlanModel>,
   ) {}
 
+  private getValidObjectId(_id: string) {
+    if (!mongoose.Types.ObjectId.isValid(_id)) {
+      throw new Error("Invalid ID");
+    }
+    return _id;
+  }
+
   async findById(id: string): Promise<WorkPlanModel> {
-    return await this._model.findOne({ _id: id }).populate({
-      path: "product",
-      populate: {
-        path: "items",
-        model: "Items",
-      },
-    });
+    return await this._model
+      .findOne({ _id: this.getValidObjectId(id) })
+      .populate({
+        path: "product",
+        populate: {
+          path: "items",
+          model: "Items",
+        },
+      });
   }
 
   async deleteById(id: string): Promise<WorkPlanModel> {
-    return await this._model.findByIdAndDelete({ _id: id });
+    return await this._model.findByIdAndDelete({
+      _id: this.getValidObjectId(id),
+    });
   }
 
   async register(dto: any): Promise<any> {
@@ -56,7 +67,7 @@ export class WorkPlanRepository {
 
   async update(id: string, dto: WorkPlanInterface): Promise<WorkPlanModel> {
     return await this._model.findByIdAndUpdate(
-      { _id: id },
+      { _id: this.getValidObjectId(id) },
       { $set: { ...dto } },
       { new: true },
     );
